0741556fccdd58da203006631e938cd507a26cf3
Виталий Якимов <fess.888@mail.ru>
2026-07-22 00:09:13 +0600

    A custom command has been created to add test products. Additional task completed

--- config/urls.py
+++ config/urls.py
@@ -23,8 +23,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('contacts/', include('catalog.urls'), name='contacts')
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
