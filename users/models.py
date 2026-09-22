9bd02c61ccd57e5eadb22e8489aa5a8f538ddfe1
Виталий Якимов <fess.888@mail.ru>
2026-09-22 17:09:12 +0600

    add models.py and urls.py in users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    avatar = models.ImageField(upload_to="users_avatars/", blank=True, null=True, verbose_name="Аватар")
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name="Номер телефона")
+    country = models.CharField(max_length=100, blank=True, null=True, verbose_name="Страна")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
+
